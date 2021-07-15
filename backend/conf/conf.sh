b90000cdedb1a02ebdf521c377572fa3095ca19b
Todor Lubenov <tlubenov@gmail.com>
2021-07-15 10:38:47 +0300

    update conf.sh script

--- backend/conf/conf.sh
+++ backend/conf/conf.sh
@@ -1,2 +1,7 @@
 
 sudo cp dronekarta.service /etc/systemd/system/
+sudo systemctl start dronekarta.service
+sudo systemctl enable dronekarta.service
+sudo systemctl restart dronekarta.service
+sudo systemctl status dronekarta.service
+
