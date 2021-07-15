fd1a62e8360dbea11399d736e47e9d58d59e59f3
Todor Lubenov <tlubenov@gmail.com>
2021-07-15 11:17:48 +0300

    add some html templates

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,13 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def get_welcome():
-    return {
-        'welcome': 'DroneKarta.com'
-    }
+    render_template('home.html')
+    #return {
+    #    'welcome': 'DroneKarta.com'
+    #}
+
+
+@app.route('/technology')
+def get_technology():
+    render_template('technology.html')
+
+
+@app.route('/support')
+def get_support():
+    render_template('support.html')
+
 
 @app.route('/service')
 def get_service():
