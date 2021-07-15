9f0c2a3e822617dea838e595c49b2720db8c02ad
Todor Lubenov <tlubenov@gmail.com>
2021-07-15 10:45:34 +0300

    define nginx and edit conf.sh

--- backend/conf/conf.sh
+++ backend/conf/conf.sh
@@ -3,5 +3,13 @@ sudo cp dronekarta.service /etc/systemd/system/
 sudo systemctl start dronekarta.service
 sudo systemctl enable dronekarta.service
 sudo systemctl restart dronekarta.service
-sudo systemctl status dronekarta.service
+
+sudo cp dronekarta /etc/nginx/sites-available/
+sudo ln -s /etc/nginx/sites-available/myproject /etc/nginx/sites-enabled
+sudo nginx -t
+sudo systemctl restart nginx
+
+sudo ufw delete allow 5000
+sudo ufw allow 'Nginx Full'
+
 
